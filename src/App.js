3977abca8094ac221a99e3d7d8d357269d218057
Asher <asher_3@hotmail.co.uk>
2020-07-30 18:30:28 +0100

    added useState for recipes as well as search functionality. Working on query response for searching recipes through the API

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
 
 const App = () => {
   const APP_ID = "e1122621";
   const APP_KEY = "e9b086109a02f466d7b7b602ffc466df";
 
-  useEffect(() => {}, []);
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
+  useEffect(() => {
+    getRecipes();
+  }, [search]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
-    const data = response.json();
-    console.log(data);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
   };
 
   return (
     <div className="App">
       <form className="search-form">
-        <input className="search-bar" type="text" />
+        <input className="search-bar" type="text" value={search} onChange={} />
         <button className="search-button" type="submit">
           Search
         </button>
       </form>
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 };
